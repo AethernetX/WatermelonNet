710e20062dbda4b9b216930da419debbc7740116
Tam <mthanimr24@gmail.com>
2024-12-06 14:33:05 +0000

    Basic hello world

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello portfolio");
+//import
+var express = require("express");
+
+var ejs = require("ejs");
+
+var mysql = require("mysql2");
+
+const app = express();
+const port = 8000;
+
+app.set("view engine", "ejs");
+
+//route handlers
+const mainRoutes = require("./routes/main");
+app.use('/', mainRoutes);
+
+app.listen(port, () => console.log(`Node app listening on port ${port}!`));
---
+++ routes/main.js
@@ -0,0 +1,10 @@
+// Create a new router
+const express = require("express")
+const router = express.Router()
+
+// Handle our routes
+router.get('/',function(req, res, next){
+    res.render('index.ejs')
+})
+
+module.exports = router;
