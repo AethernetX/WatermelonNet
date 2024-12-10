58709360a4c246bc5cbb2b1f82aa2a496256fb65
Tam <mthanimr24@gmail.com>
2024-12-10 14:27:38 +0000

    database + express validator added

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ dotenv.config();
 
 //import
 var express = require("express");
-
 var ejs = require("ejs");
-
 var mysql = require("mysql2");
 
 const app = express();
@@ -14,8 +12,30 @@ const port = 8000;
 
 app.set("view engine", "ejs");
 
+// Set up the body parser 
+app.use(express.urlencoded({ extended: true }))
+
 //public folder
 app.use(express.static(__dirname + '/public'));
+
+// setup express sanitiser
+app.use(expressSanitizer());
+
+// Define the database connection
+const db = mysql.createConnection ({
+    host: 'localhost',
+    user: 'melon_app',
+    password: process.env.DB_PASSWORD,
+    database: 'melon_trade'
+});
+// Connect to the database
+db.connect((err) => {
+    if (err) {
+        throw err
+    }
+    console.log('Connected to database');
+});
+global.db = db
 
 //route handlers
 const mainRoutes = require("./routes/main");
