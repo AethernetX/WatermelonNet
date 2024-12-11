1d007a9e4bfd9137da27337be2610e2637b6db8c
Tam <mthanimr24@gmail.com>
2024-12-11 17:53:52 +0000

    tweaks + new table

--- create_db.sql
+++ create_db.sql
@@ -3,7 +3,9 @@ CREATE DATABASE IF NOT EXISTS melon_trade;
 USE melon_trade;
 
 # create the tables
-CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT primary key NOT NULL, email VARCHAR(40), username VARCHAR(14), isAdmin BOOL DEFAULT FALSE, password VARCHAR(70));
+CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT primary key NOT NULL, email VARCHAR(40), username VARCHAR(14), isAdmin BOOL DEFAULT FALSE, password VARCHAR(70), purse DECIMAL(5,2) unsigned DEFAULT 100, melons int unsigned DEFAULT 0, land int unsigned DEFAULT 100, cycles int unsigned DEFAULT 0);
+
+CREATE TABLE IF NOT EXISTS items (id INT AUTO_INCREMENT primary key NOT NULL, name VARCHAR(40), growth DECIMAL DEFAULT 0, farm rate INT DEFAULT 0, price DECIMAL(5,2) unsigned DEFAULT 1);
 
 # create the app user
 CREATE USER IF NOT EXISTS 'melon_app'@'localhost' IDENTIFIED BY 'skibidi'; 
