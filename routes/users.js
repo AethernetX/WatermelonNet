b6019e3facfd1a73f1fd0380413da811b832eb71
Tam <mthanimr24@gmail.com>
2024-12-29 18:29:27 +0000

    items and machines show up on profile + included in networth calc

--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,7 @@ router.get("/", (req, res, next) => {
 router.get("/:username", (req, res, next) => {
     let username = req.params.username;
     req.sanitize(username);    
-    db.query("SELECT username, purse, melons, land FROM users WHERE username = ?", [username], (err, result) => {
+    db.query("SELECT username, id, purse, melons, land FROM users WHERE username = ?", [username], (err, result) => {
         if(err){
             next(err);
         }
@@ -24,15 +24,40 @@ router.get("/:username", (req, res, next) => {
         if(result.length == 0){
             res.send("User does not exist!")
         } else {
-            request("https://api.fxratesapi.com/latest", (error, response, body) => {
-                if(error){
-                    next(error);
-                }
-                let rates = JSON.parse(body).rates.GBP;
-                rates = rates.toFixed(2) * 5;
 
-                res.render("profile.ejs", {user: result[0], rates: rates});
-            });
+            db.query("SELECT * FROM items_users INNER JOIN items ON items_users.item_id = items.id WHERE items_users.user_id = ?", [result[0].id], (item_err, item_result) =>{
+                if(item_err){
+                    next(err);
+                }
+
+                db.query("SELECT * FROM machines_users INNER JOIN machines ON machines_users.machine_id = machines.id WHERE machines_users.user_id = ?", [result[0].id], (machine_err, machine_result) => {
+                    if(machine_err){
+                        next(err);
+                    }
+
+                    request("https://api.fxratesapi.com/latest", (error, response, body) => {
+                        if(error){
+                            next(error);
+                        }
+
+                        let rates = JSON.parse(body).rates.GBP;
+                        rates = rates.toFixed(2) * 5;
+
+                        let assets = 0;
+
+                        for(item in item_result){
+                            assets += Number(item_result[item].price);
+                        }
+
+                        for(machine in machine_result){
+                            assets += Number(machine_result[machine].price);
+                        }
+
+                        res.render("profile.ejs", {user: result[0], rates: rates, items: item_result, machines: machine_result, assets: assets});
+                    });
+
+                });
+            }); 
         }
     });
 });
