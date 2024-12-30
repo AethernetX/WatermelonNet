6b519aee8d99fb55f8bd2f9a523a06d9a27e4886
Tam <mthanimr24@gmail.com>
2024-12-30 17:26:12 +0000

    minor fixes

--- create_db.sql
+++ create_db.sql
@@ -10,8 +10,10 @@ USE melon_trade;
 
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT primary key NOT NULL,
-    email VARCHAR(40), username VARCHAR(14),
-    isAdmin BOOL DEFAULT FALSE, password VARCHAR(70),
+    email VARCHAR(40), 
+    username VARCHAR(14),
+    isAdmin BOOL DEFAULT FALSE, 
+    password VARCHAR(70),
     purse DECIMAL(20,2) unsigned DEFAULT 100,
     melons int unsigned DEFAULT 0,
     land int unsigned DEFAULT 100,
@@ -32,11 +34,11 @@ CREATE TABLE IF NOT EXISTS items (
 -- -----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS machines (
-    id INT AUTO_INCREMENT primary key NOT NULL, 
-    name VARCHAR(40), 
-    farm_rate INT DEFAULT 0, 
-    fuel INT DEFAULT 1, 
-    price DECIMAL(10,2) unsigned DEFAULT 1);
+    id int auto_increment primary key not null, 
+    name varchar(40), 
+    farm_rate int default 0, 
+    fuel int default 1, 
+    price decimal(10,2) unsigned default 1);
 
 -- -----------------------------------------------------
 -- Table items_users
